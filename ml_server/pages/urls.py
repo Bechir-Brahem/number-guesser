f78369cd368586c375bfc5f069c051ce3323946c
Bechir-Brahem <bachbrahem@gmail.com>
2022-03-29 00:46:32 +0100

    setup deploy branch

--- ml_server/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import process, listModels
-
-urlpatterns = [
-    path('process', process, name='process'),
-    path('list', listModels, name='listModels')
-]
